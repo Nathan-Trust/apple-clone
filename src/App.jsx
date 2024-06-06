a272d51eea17f9733d1ed03645eb8f21bb5b53cf
Nathan-Trust <nathantrustekanem@gmail.com>
2024-06-06 06:17:21 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlight from './components/Highlight'
+// import Model from './components/Model'
+
+function App() {
+
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlight />
+      {/* <Model/> */}
+   </main>
+  )
+}
+
+export default App
